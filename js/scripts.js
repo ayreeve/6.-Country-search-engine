aa8b292886199cfff28849577822d1f214806d19
Kaja <ayreeve@wp.pl>
2017-06-13 13:38:26 +0200

    var names changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 // jslint devel: true
 
-var url = 'https://restcountries.eu/rest/v1/name/',
-    countriesList = $('#countries');
+var $url = 'https://restcountries.eu/rest/v1/name/',
+    $countriesList = $('#countries');
 
 function showCountriesList(resp) {
-    countriesList.empty();
+    $countriesList.empty();
     resp.forEach(function (item) {
-        $('<li>').text(item.name + " (" + item.nativeName + ")" + " - capital city: " + item.capital).appendTo(countriesList);
+        $('<li>').text(item.name + ' (' + item.nativeName + ')' + ' - capital city: ' + item.capital).appendTo($countriesList);
     });
 }
 
 function incorrectCountry(resp) {
-    countriesList.empty();
-    $('<p>').text("There is no such country on Earth (or you don't use english country name)").appendTo(countriesList).css("text-align", "center");
+    $countriesList.empty();
+    $('<p>').text('There is no such country on Earth (or you don\'t use english country name)').appendTo($countriesList).css('text-align', 'center');
 }
 
 function searchCountries() {
-    var countryName = $('#country-name').val();
+    var $countryName = $('#country-name').val();
 
     $.ajax({
-        url: url + countryName,
+        url: $url + $countryName,
         method: 'GET',
         success: showCountriesList,
         error: function errorActions() {
-            if (countryName === "") {
-                countriesList.empty();
-                $('<p>').text("Type the name of the country in the filed above").appendTo(countriesList).css("text-align", "center");
+            if ($countryName === '') {
+                $countriesList.empty();
+                $('<p>').text('Type the name of the country in the filed above').appendTo($countriesList).css('text-align', 'center');
             } else {
                 incorrectCountry();
             }
